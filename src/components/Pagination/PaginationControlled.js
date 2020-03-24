2d5168d03b39e8e0a94defc41f19d73a9fb87274
Andrey <35691656+fufylev@users.noreply.github.com>
2020-03-24 17:27:01 +0300

    New features: header, pagination, pokimons per page, etc.
    1. Applied Material UI package
    2. Created a pagination block
    3. Header

---
+++ src/components/Pagination/PaginationControlled.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Pagination from '@material-ui/lab/Pagination';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        '& > * + *': {
+            marginTop: theme.spacing(2),
+        },
+    },
+}));
+
+export default function PaginationControlled(props) {
+    const classes = useStyles();
+    const [page, setPage] = useState(1);
+
+    const {pagesToShow} = props;
+
+    const numberOfPagesToPaginate = Math.ceil(964/pagesToShow);
+
+    const handleChange = (event, value) => {
+        setPage(value);
+        props.setCurrntPage(value);
+    };
+
+    return (
+        <div className={classes.root}>
+            <Pagination count={numberOfPagesToPaginate} page={page} onChange={handleChange} />
+        </div>
+    );
+}
+
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import './index.scss';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+import Header from "./components/Header/Header";
+import { DashBoard } from "./components/layout/DashBoard/DashBoard";
 
 class App extends Component {
     render() {
         return (
-            <div className="container">
-                <h1>Hello world!</h1>
+            <div>
+                <Header/>
+                <DashBoard/>
             </div>
         );
     }
