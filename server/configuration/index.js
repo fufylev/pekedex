7256ca081dbe065693a6959a452cfeaeaef2f076
Andrey <35691656+fufylev@users.noreply.github.com>
2020-03-30 21:40:12 +0300

    minor refactoring
    minor refactoring

--- server/configuration/index.js
+++ server/configuration/index.js
@@ -2,12 +2,12 @@ module.exports = {
   JWT_SECRET: 'The doors are open for those how are bold enough to knock',
   oauth: {
     google: {
-      clientID: '245233248634-fvk04phkvc0thk32iiugofspmvfardkj.apps.googleusercontent.com',
-      clientSecret: 'Y-7ryF9WKC_lWSRlHFgdryI-'
+      clientID: '',
+      clientSecret: ''
     },
     facebook: {
-      clientID: '210062593585689',
-      clientSecret: '7943cb1cfeefa2776a750f7f6479eb19'
+      clientID: '',
+      clientSecret: ''
     }
   }
 }
